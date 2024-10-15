a69d2335729aef09105e53609c63cf9b8bb41a9f
Jakub Werner <jakubwerner1@gmail.com>
2024-10-15 20:56:07 +0200

    slight design update

--- ipq-frontend/components/MainQuestionWindow.vue
+++ ipq-frontend/components/MainQuestionWindow.vue
@@ -1,13 +1,12 @@
 <template>
-  <v-container style="max-width: 1000px; margin: auto; max-height: 610px">
-    <v-container style="transform: scale(0.9); transform-origin: top left">
-      <!-- Container now has margin auto for centering -->
-      <v-row justify="center">
-        <GuessQuestionWindow v-if="!questionStore.reviewMode"></GuessQuestionWindow>
-        <ReviewQuestionWindow v-else></ReviewQuestionWindow>
-        <QuestionNavigation ></QuestionNavigation>
-      </v-row>
-    </v-container>
+  <v-container fluid class="main-container">
+    <v-row justify="center">
+      <v-col cols="12" sm="10" md="8" lg="6">
+        <GuessQuestionWindow v-if="!questionStore.reviewMode" />
+        <ReviewQuestionWindow v-else />
+        <QuestionNavigation />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -29,18 +28,16 @@ export default defineComponent({
     const questionStore = useQuestionStore();
     return {
       questionStore,
-      GuessQuestionWindow,
-      ReviewQuestionWindow,
-      QuestionNavigation,
     };
   },
 });
 </script>
 
 <style scoped>
-/* Optional: Ensure v-row does not have extra padding or margins */
-v-row {
-  margin: 0;
-  padding: 0;
+.main-container {
+  margin: auto;
+  padding: 16px; /* Optional: Adjust padding as needed */
 }
+
+/* Remove unnecessary styles */
 </style>
--- ipq-frontend/pages/index.vue
+++ ipq-frontend/pages/index.vue
@@ -251,10 +251,6 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  color: #42b983;
-}
-
 .v-layout {
   display: flex;
   flex-direction: column;
