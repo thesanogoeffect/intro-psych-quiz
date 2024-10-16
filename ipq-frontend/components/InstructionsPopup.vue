36e140770d960a29b9c40b1eda27da8b8ed05d8e
Jakub Werner <jakubwerner1@gmail.com>
2024-10-17 00:11:21 +0200

    small formatting changes for the popups

--- ipq-frontend/components/InstructionsPopup.vue
+++ ipq-frontend/components/InstructionsPopup.vue
@@ -1,54 +1,55 @@
 <template>
-  <v-dialog v-model="generalStore.instructionsPopup" max-width="800px">
-    <v-card>
-      <v-card-title class="headline text-center">Help</v-card-title>
-      <v-card-text class="mx-4">
-        <p><strong>Instructions:</strong></p>
-        <ol>
-          <li>
-            Use the left-right arrows (or A and D keys) to navigate through
-            questions.
-          </li>
-          <li>
-            The right arrow works as a "Skip" button if you don't know the
-            answer. You only get 3 skips, but you might earn more by doing well.
-          </li>
-          <li>
-            Flag questions that seem incorrect so they can be reviewed by an LLM
-            or me.
-          </li>
-          <li>
-            You can upvote/downvote questions to improve question quality for
-            everyone.
-          </li>
-        </ol>
+    <v-dialog v-model="generalStore.instructionsPopup" max-width="800px">
+        <v-card>
+            <v-card-title class="headline text-center">Help</v-card-title>
+            <v-card-text class="mx-4">
+                <h3><strong>Instructions:</strong></h3>
+                <ol>
+                    <li>
+                        Use the left-right arrows (or A and D keys) to navigate through
+                        questions.
+                    </li>
+                    <li>
+                        The right arrow works as a "Skip" button if you don't know the
+                        answer. You only get 3 skips, but you might earn more by doing well.
+                    </li>
+                    <li>
+                        Flag questions that seem incorrect so they can be reviewed by an LLM
+                        or others.
+                    </li>
+                    <li>
+                        You can upvote/downvote questions to improve question quality for
+                        everyone.
+                    </li>
+                </ol>
 
-        <p><strong>Top menu:</strong></p>
-        <ul>
-          <li>You can filter questions by chapters or by source.</li>
-          <li>
-            There is an About page, where you can find more info and how to
-            contact me if needed.
-          </li>
-          <li>
-            The dark mode is under construction, but you can try it in the top
-            menu!
-          </li>
-          <li>
-            You can toggle the stats/info sidebars by clicking the icons on the
-            sides.
-          </li>
-        </ul>
-
-        <v-btn color="primary" @click="closeDialog">Sounds good</v-btn>
-        <br />
-        <p class="">
-          Good luck, <br />
-          Jakub
-        </p>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+                <h3><strong>Top menu:</strong></h3>
+                <ul>
+                    <li>You can filter questions by chapters or by source.</li>
+                    <li>
+                        There is an About page, where you can find more info and how to
+                        contact me if needed.
+                    </li>
+                    <li>
+                        The dark mode is under construction, but you can try it in the top
+                        menu!
+                    </li>
+                    <li>
+                        You can toggle the stats/info sidebars by clicking the icons on the
+                        sides.
+                    </li>
+                </ul>
+                <v-container class="text-center">
+                <v-btn color="primary" @click="closeDialog">Sounds good</v-btn>
+                </v-container>
+                <br />
+                <p>
+                    Enjoy! <br />
+                    Jakub
+                </p>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
 </template>
   
   <script>
--- ipq-frontend/components/LandingPopup.vue
+++ ipq-frontend/components/LandingPopup.vue
@@ -10,7 +10,7 @@
         <p>
           You can find instructions and more info in the top menu.
         </p>
-        <p><strong>Disclaimer:</strong></p>
+        <h3><strong>Disclaimer:</strong></h3>
         <p>
           Some of the questions <b>might be incorrect</b>, please flag them if you
           think so.
@@ -19,17 +19,19 @@
           As the app is in a preview state, there might be some issues. If you
           find any, please let me know.
         </p>
-        <p><strong>Current Status:</strong></p>
+        <h3><strong>News:</strong></h3>
         <ul>
           <li>
             We are getting closer to a full release on 21-10-2024!
-            The app currently still doesn't work on mobile, but I'm working on it.
+            The app currently still doesn't work on mobile, but I'm working on it 💪
           </li>
         </ul>
+        <v-container class="text-center">
         <v-btn color="primary" @click="closeDialog">Sounds good</v-btn>
+        </v-container>
         <br />
         <p>
-          Good luck, <br />
+          Enjoy! <br />
           Jakub
         </p>
       </v-card-text>
